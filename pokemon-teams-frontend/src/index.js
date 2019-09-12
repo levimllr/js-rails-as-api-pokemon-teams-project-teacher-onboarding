d070d380c25a11838849fb396a4dbe1b9a1b13d5
github username <github email address>
2019-09-11 18:25:55 -0700

    First API fetch written

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,15 @@
-const BASE_URL = "http://localhost:3000"
-const TRAINERS_URL = `${BASE_URL}/trainers`
-const POKEMONS_URL = `${BASE_URL}/pokemons`
+const BASE_URL = "http://localhost:3000";
+const TRAINERS_URL = `${BASE_URL}/trainers`;
+const POKEMONS_URL = `${BASE_URL}/pokemons`;
+
+document.addEventListener('DOMContentLoaded', loadTrainers);
+
+function loadTrainers() {
+    console.log("Loading trainers...")
+    fetch(TRAINERS_URL)
+        .then(resp => resp.json())
+        .then(json => {console.log(json)    });  
+};
+
+
+
